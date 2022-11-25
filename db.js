f2f59316f42122d8eb76fb6a5728724ae13119b7
Victor <72002526+victor-ivanov-ivt20-2@users.noreply.github.com>
2022-11-26 02:41:37 +0900

    ДОбавил связь с базами данных

--- db.js
+++ db.js
@@ -1,4 +1,62 @@
-//GMwqbJYpJW3B
+
+
+
+const db_drop_table = () => {
+    const mysql = require('mysql');
+    const connection = mysql.createConnection({
+        host: '79.141.66.70',
+        port: '6033',
+        user: 'stef',
+        password: 'password',
+        database: 'leanevent_db'
+    });
+
+    connection.connect();
+
+    connection.query("DROP TABLE registrations", (error, results) => {
+        if (error) throw error;
+        console.log(results);
+    })
+
+    connection.end();
+}
+
+
+const db_create_table = () => {
+    
+
+    const mysql = require('mysql');
+    const connection = mysql.createConnection({
+        host: '79.141.66.70',
+        port: '6033',
+        user: 'stef',
+        password: 'password',
+        database: 'leanevent_db'
+    });
+
+    connection.connect();
+    
+    connection.query("CREATE TABLE registrations (name VARCHAR(255), regions VARCHAR(255), email VARCHAR(255), chatID INT)", (error, results) => {
+        if (error) throw error;
+        console.log(results);
+    })
+
+    connection.end();
+
+    //SHOW TABLES FROM 'db_name' LIKE 'нужная_таблица';
+}
+
+
+const db_add_team = () => {
+
+}
+
+
+module.exports = {
+    db_create_table, 
+    db_add_team,
+    db_drop_table
+}
 
 // const {Sequelize} = require('sequelize');
 
@@ -13,22 +71,22 @@
 //     }
 // )
 
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: '79.141.66.70',
-    user: 'leanevent',
-    password: 'GMwqbJYpJW3B',
-    database: 'leanevent_db'
-});
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//     host: '79.141.66.70',
+//     user: 'stef',
+//     password: 'password',
+//     database: 'leanevent_db'
+// });
 
-connection.connect();
-// let data = "S";
-connection.query('SELECT1+1  AS solution', function (error, results, fields) {
-    if (error) throw error;
-    data = 'The solution is: ' + results[0].solution;
-});
+// connection.connect();
+// // let data = "S";
+// connection.query('SELECT1+1  AS solution', function (error, results, fields) {
+//     if (error) throw error;
+//     data = 'The solution is: ' + results[0].solution;
+// });
 
-connection.end();
+// connection.end();
 
 // module.exports = {
 //     data
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramApi = require('node-telegram-bot-api')
 // const { data } = require('./db.js')
 const token = '5824203782:AAGfMMRUygCYl3hYL8-atmR9Y_1DqI4v95g'
-
+const { db_create_table, db_add_team, db_drop_table } = require('./db.js')
 const bot = new TelegramApi(token, { polling: true })
 
 const someOptions = {
@@ -15,61 +15,168 @@ const someOptions = {
     })
 }
 
-const db_test = () => {
-    var mysql = require('mysql');
-    var connection = mysql.createConnection({
+// db_create_table();
+// db_drop_table();
+
+let adminID = 342559638;
+let chats = []
+const db_all_chatID = () => {
+    const mysql = require('mysql');
+    const connection = mysql.createConnection({
         host: '79.141.66.70',
         port: '6033',
-        user: 'leanevent',
-        password: 'GMwqbJYpJW3B',
+        user: 'stef',
+        password: 'password',
         database: 'leanevent_db'
     });
 
     connection.connect();
-    // let data = "S";
-    // connection.query('SELECT1+1  AS solution', function (error, results, fields) {
-    //     if (error) console.log(error);
-    //     data = 'The solution is: ' + results[0].solution;
-    // });
-
-
-
-
-    console.log("hey!")
+    connection.query("select distinct chatID from registrations", function (error, results, fields) {
+            if (error) throw error;
+            chats = results;
+            noti();
+            console.log('The solutions is: ' + results)
+        })
     connection.end();
 }
-db_test();
+
+
+const db_create_team = (form) => {
+    const mysql = require('mysql');
+    const connection = mysql.createConnection({
+        host: '79.141.66.70',
+        port: '6033',
+        user: 'stef',
+        password: 'password',
+        database: 'leanevent_db'
+    });
+
+    connection.connect();
+    connection.query("INSERT INTO registrations (name, regions, email, chatID) VALUES ('" +
+        form.name + "', '" +
+        form.regions + "' , '" +
+        form.email + "', '" +
+        form.chatID + "')", function (error, results, fields) {
+            if (error) throw error;
+            console.log('The solutions is: ' + results)
+        })
+    
+    connection.end();
+}
+
+const noti = async () => {
+    console.log(chats[0].chatID)
+    for (let i = 0; i < chats.length; i++) {
+        bot.sendMessage(chats[i].chatID, "Разбудить Андрея в 7ч")
+    }
+
+}
+
 
 const start = async () => {
-
-
+    let checker = 0;
+    let form = {
+        name: undefined,
+        regions: undefined,
+        email: undefined,
+        chatID: undefined
+    }
     bot.setMyCommands([
         {
             command: '/start', description: 'Начать'
         },
         {
+            command: '/stop', description: 'Остановить регистрацию'
+        },
+        {
             command: '/info', description: 'Подробнее'
+        },
+        {
+            command: '/register', description: 'Регистрация команды'
         }
     ])
+
     bot.on('message', async msg => {
         const text = msg.text;
         const chatId = msg.chat.id;
+        if (chatId === adminID) {
+            bot.setMyCommands([
+                {
+                    command: '/start', description: 'Начать'
+                },
+                {
+                    command: '/notiall', description: 'Всем написать'
+                }
+            ])
+        }
+
+        if (text === "/notiall" && chatId === adminID) {
+            db_all_chatID();
+        }
+        if (text === "/stop") {
+            checker = 0;
+            return bot.sendMessage(chatId, 'Пока!')
+        }
+        if (checker === 1) {
+            form.name = text;
+            checker = 2;
+            return bot.sendMessage(chatId, 'Укажите ваш email адрес!')
+        }
+        if (checker === 2) {
+            form.email = text;
+            checker = 3;
+            return bot.sendMessage(chatId, 'Укажите ваш регион')
+        }
+        if (checker === 3) {
+            form.regions = text;
+            checker = 0;
+            console.log(form)
+            db_create_team(form)
+            return bot.sendMessage(chatId, 'Данные отправлены!')
+        }
+        if (text === "/register") {
+            form.chatID = chatId;
+            checker = 1;
+            return bot.sendMessage(chatId, 'Какое название вашей команды?')
+        }
         if (text === "/start") {
-            return bot.sendMessage(chatId, 'Добро пожаловать в телеграм бот Lean Event!')
+            return bot.sendMessage(chatId, 'Добро пожаловать в телеграм бот Lean Event! Чтобы зарегистрировать команду, напишите /register')
         }
         if (text === "/info") {
             return bot.sendMessage(chatId, 'Lean Event поможет организаторам МПИТ, автоматизировать соревнования')
         }
-        return bot.sendMessage(chatId, "Я тебя не понимаю", someOptions)
     })
 
-    bot.on('callback_query', msg => {
-        const data = msg.data;
-        const chatId = msg.message.chat.id;
 
-        bot.sendMessage(chatId, "Спасибо за ответ!")
-        return console.log(msg)
-    })
+    // bot.onText(/команда (.+)/, async (msg, match) => {
+    //     const chatId = msg.from.id;
+    //     const text = match[1];
+    //     if (text.length < 2) {
+    //         return bot.sendMessage(chatId, "Название слишком короткое")
+    //     }
+    //     db_create_team(text, chatId);
+    //     bot.sendMessage(chatId, 'Отлично! Ваша команда ' + text);
+    // })
+    // bot.on('message', async msg => {
+    //     const text = msg.text;
+    //     const chatId = msg.chat.id;
+    //     if (text === "/start") {
+
+    //         return bot.sendMessage(chatId, 'Добро пожаловать в телеграм бот Lean Event!')
+    //     }
+    //     if (text === "/info") {
+    //         return bot.sendMessage(chatId, 'Lean Event поможет организаторам МПИТ, автоматизировать соревнования')
+    //     }
+    //     // return bot.sendMessage(chatId, "Я тебя не понимаю", someOptions)
+    // })
+
+    // bot.on('callback_query', msg => {
+    //     const data = msg.data;
+    //     const chatId = msg.message.chat.id;
+
+    //     bot.sendMessage(chatId, "Спасибо за ответ!")
+    //     return console.log(msg)
+    // })
 }
 
 start();
